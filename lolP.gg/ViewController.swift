25b52ea98737a83b21c2e3dc780b2a2b4973bd9a
zoop1259 <kdm432191@gmail.com>
2021-12-02 23:29:03 +0900

    Initial Commit

---
+++ lolP.gg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lolP.gg
+//
+//  Created by 강대민 on 2021/12/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
