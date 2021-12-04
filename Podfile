a3406d6a226b8c587d5906be9d5749384e255e53
zoop1259 <kdm432191@gmail.com>
2021-12-05 05:12:57 +0900

    유레카.. 탭바+네비게이션 컨트롤러 ..

--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ target 'lolP.gg' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
   pod 'SwiftyJSON', '~> 4.0'
-
+  pod 'Alamofire', '~>4.7'
   # Pods for lolP.gg
 
 end
--- lolP.gg/ViewController.swift
+++ lolP.gg/ViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
@@ -7,42 +8,11 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
 
     @IBOutlet var CollectionViewMain: UICollectionView!
-    
-    var champData :Array<Dictionary<String, Any>>?
-    
-    func getChamps() {
-        //이게 1번
-        let task = URLSession.shared.dataTask(with: URL(string: "https://ddragon.leagueoflegends.com/cdn/11.23.1/data/ko_KR/champion.json")!) { data, response, error in
-            //첫번째 문법이 나온다. 옵셔널 바인딩
-            
-            //이게 2번
-            if let dataJson = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: dataJson, options:[]) as! Dictionary<String, Any>
-                    //스위프트에선 Dictionary라고 한다.
-                    let names = json["freeChampionIds"] as! Array<Dictionary<String, Any>>
-                    //여기서 self가 빠지면 위치를 모른다. 그렇기 떄문에 self를 사용한다.
-                    self.champData = names
-                
-                    //메인에서 일을하게 만들어야한다. 외워야 하는 문법
-                    DispatchQueue.main.async {
-                        //이것이 4번 통보하는 법. reloadData
-                        self.CollectionViewMain.reloadData()
-                    }
-                }
-                catch {}
-            }
-        }
-        task.resume()
-    }
+
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if let champs = champData {
-            return champs.count
-        }
-        else {
-            return 0
-        }
+
+        return nameList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -50,19 +20,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             return UICollectionViewCell()
         }
         
-        let idx = indexPath.row
-        //newsData가 비어있지 않으면
-        if let names = champData {
-            //news의 indexPath의 row의 만큼 뉴스를 가져와서
-            let row = names[idx]
-            //그 news의 정보가 dictionary형태면
-            if let r = row as? Dictionary<String, Any> {
-                //거기서 title을 가져와라.
-                if let title = r["freeChampionIds"] as? String {
-                cell.nameLabel.text = title
-                }
-            }
-        }
+
         return cell
     }
     
@@ -79,7 +37,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        getChamps()
         
     }
 
