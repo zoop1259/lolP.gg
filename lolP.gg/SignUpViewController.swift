efa39ee6563eb00a04fb1db7b74acd1ffd6089d2
zoop1259 <kdm432191@gmail.com>
2021-12-22 18:30:47 +0900

    왜 오류가 나는가...

---
+++ lolP.gg/SignUpViewController.swift
@@ -0,0 +1,129 @@
+//
+//  SignUpViewController.swift
+//  lolP.gg
+//
+//  Created by 강대민 on 2021/12/22.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var txtUserEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtPasswordConfirm: UITextField!
+    @IBOutlet weak var lblPasswordConfirmed: UILabel!
+    @IBOutlet weak var imgProfilePicture: UIImageView!
+
+    //??
+    var ref: DatabaseReference!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 텍스트 필드에 대한 딜리게이트, 데이터소스 연결 - 유효성 검사에서 필요함
+        txtUserEmail.delegate = self
+        txtPassword.delegate = self
+        txtPasswordConfirm.delegate = self
+        
+        // 패스워드 일치 여부를 표시하는 레이블을 빈 텍스트로
+        lblPasswordConfirmed.text = ""
+        
+        
+    }
+    
+    @IBAction func btnActCancel(_ sender: UIButton) {
+        //액션 세그로 이어진 뷰컨트롤러를 사라지게해서 되돌림.
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func btnActReset(_ sender: UIButton) {
+        //초기값으로
+        txtUserEmail.text = ""
+        txtPassword.text = ""
+        txtPasswordConfirm.text = ""
+        lblPasswordConfirmed.text = ""
+        // -- 사진 초기화 --
+        
+    }
+    @IBAction func btnActSubmit(_ sender: UIButton) {
+        //파이어베이스에 정보를 보내야됨.
+        guard let userEmail = txtUserEmail.text,
+              let userPassword = txtPassword.text,
+              let userPasswordConfirm = txtPasswordConfirm.text else {
+            return
+        }
+        
+        guard userPassword != ""
+                && userPasswordConfirm != ""
+                && userPassword == userPasswordConfirm else {
+                    let alert = UIAlertController(title: "PW Error", message: "패스워드가 일치하지 않습니다.", preferredStyle: .alert)
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: userEmail, password: userPassword) { [self] authResult, error in
+            // 이메일, 비밀번호 전송
+            guard let user = authResult?.user, error == nil else {
+                let alert = UIAlertController(title: "Error", message: error!.localizedDescription , preferredStyle: .alert)
+                return
+            }
+            
+            // 파이어베이스에 추가정보 입력할떄..? id라던가..
+            //ref.child("users").child(user.uid).setValue(["interesting": selectedInteresting])
+            
+            let confirm = UIAlertController(title: "Complete", message: "\(user.email!) 님의 회원가입이 완료되었습니다.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler : nil )
+            confirm.addAction(okAction)
+            
+            present(confirm, animated: true, completion: nil)
+        }
+    }
+}
+
+//비밀번호 일치 확인하기 위한 delegate 설정
+extension SignUpViewController: UITextFieldDelegate {
+    
+    func setLabelPasswordConfirm(_ password: String, _ passwordConfirm: String)  {
+        
+        guard passwordConfirm != "" else {
+            lblPasswordConfirmed.text = ""
+            return
+        }
+        
+        if password == passwordConfirm {
+            lblPasswordConfirmed.textColor = .green
+            lblPasswordConfirmed.text = "패스워드가 일치합니다."
+        } else {
+            lblPasswordConfirmed.textColor = .red
+            lblPasswordConfirmed.text = "패스워드가 일치하지 않습니다."
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        switch textField {
+        case txtUserEmail:
+            txtPassword.becomeFirstResponder()
+        case txtPassword:
+            txtPasswordConfirm.becomeFirstResponder()
+        default:
+            textField.resignFirstResponder()
+        }
+        
+        return false
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField == txtPasswordConfirm {
+            guard let password = txtPassword.text,
+                  let passwordConfirmBefore = txtPasswordConfirm.text else {
+                return true
+            }
+            let passwordConfirm = string.isEmpty ? passwordConfirmBefore[0..<(passwordConfirmBefore.count - 1)] : passwordConfirmBefore + string
+            setLabelPasswordConfirm(password, passwordConfirm)
+            
+        }
+        return true
+    }
+}
