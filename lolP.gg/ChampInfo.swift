15f8f8d593779fa2d549a0532bb1959d74bfe251
zoop1259 <kdm432191@gmail.com>
2022-04-17 23:53:30 +0900

    회원가입 마무리중.

--- lolP.gg/ChampInfo.swift
+++ lolP.gg/ChampInfo.swift
@@ -7,12 +7,13 @@
 import Foundation
 import UIKit
 
+//모든데이터
 struct MainData: Codable {
     let data: [String: ChampData]
 }
    
 // MARK: - ChampData
-    struct ChampData: Codable {
-        let id, key, name: String
-    }
+struct ChampData: Codable {
+    let id, key, name: String
+}
 
