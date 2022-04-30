50e28aeba55da7a7e5474f43d6e7b0e387ed07b8
zoop1259 <kdm432191@gmail.com>
2022-04-30 23:05:08 +0900

    ing...

--- lolP.gg/ViewController.swift
+++ lolP.gg/ViewController.swift
@@ -93,19 +93,9 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             controller.VCVersion = VCVersion
             print("챔프디테일에 넘겨주는 버전: \(VCVersion)")
         }
-        
         //이동
         show(controller, sender: nil)
     }
-    
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        let itemSpacing: CGFloat = 5 // 가로에서 cell과 cell 사이의 거리
-//        let textAreaHeight: CGFloat = 20 // textLabel이 차지하는 높이
-//        let width: CGFloat = (collectionView.bounds.width - itemSpacing)/2 // 셀 하나의 너비
-//        let height: CGFloat = width * 10/7 + textAreaHeight //셀 하나의 높이
-//
-//        return CGSize(width: width, height: height)
-//    }
   
     func champData() {
         
